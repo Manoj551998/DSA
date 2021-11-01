f0e54ec2e6d8cfe4cc020a12d0d2cddc85cf6669
Manoj_prabhu <30549738+Manoj551998@users.noreply.github.com>
2021-11-01 20:03:07 +0530

    Update Two Strings(Sub sequence).java
    Optimized TC

--- Two Strings(Sub sequence).java
+++ Two Strings(Sub sequence).java
@@ -1,22 +1,19 @@
-public static String twoStrings(String s1, String s2) {
-    // Write your code here
-    TreeSet<Character>set=new TreeSet<>();
-    boolean flag=false;
+int a1[]=new int[26];
+        boolean flag=false;
         for(int i=0;i<s1.length();i++)
         {
-            set.add(s1.charAt(i));
+            a1[s1.charAt(i)-'a']++;
         }
-    for(int i=0;i<s2.length();i++)
-    {
-        if(set.contains(s2.charAt(i)))
+        for(int i=0;i<s2.length();i++)
         {
-        flag=true;
-        break;
+            if(a1[s2.charAt(i)-'a']>0)
+            {
+                flag=true;
+                break;
+            }
         }
-    }
     if(flag)
     return "YES";
     else
     return "NO";
-
     }
